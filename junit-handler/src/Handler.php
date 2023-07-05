b46b16bde17b00a685a6407f019e391aa51b6bcf
Tim Austin <tim@neenjaw.com>
2023-07-05 10:49:42 -0600

    add test code, bump to v3 runner

--- junit-handler/src/Handler.php
+++ junit-handler/src/Handler.php
@@ -3,15 +3,17 @@
 namespace Exercism\JunitHandler;
 
 use ReflectionClass;
+use ReflectionMethod;
 use SimpleXMLElement;
 use phpowermove\docblock\Docblock;
 
-class Handler {
-    private const VERSION = 2;
-    
+class Handler
+{
+    private const VERSION = 3;
+
     private const STATUS_ERROR = 'error';
-    private const STATUS_PASS= 'pass';
-    private const STATUS_FAIL= 'fail';
+    private const STATUS_PASS = 'pass';
+    private const STATUS_FAIL = 'fail';
 
     public function run(string $xml_path, $json_path): void
     {
@@ -33,44 +35,41 @@ class Handler {
 
         $testsuite = $testsuites->testsuite;
         $testsuite_attrs = $testsuite->attributes();
-        
+
         $test_class = $testsuite_attrs['name'];
         $test_file_path = $testsuite_attrs['file'];
 
-        $testcase_error_count = (int) $testsuite_attrs['errors'];  
-        $testcase_failure_count = (int) $testsuite_attrs['failures'];  
+        $testcase_error_count = (int) $testsuite_attrs['errors'];
+        $testcase_failure_count = (int) $testsuite_attrs['failures'];
 
-        $reflection_test_class = $this->getReflectionTestClass($test_class, $test_file_path);
+        $test_file_source = explode("\n", file_get_contents($test_file_path));
+        $reflection_test_class =
+            $this->getReflectionTestClass($test_class, $test_file_path);
 
         $output = [
             'version' => self::VERSION,
             'status' => ($testcase_error_count !== 0 || $testcase_failure_count !== 0)
                 ? self::STATUS_FAIL
                 : self::STATUS_PASS,
-            'tests' => $this->parseTestCases($testsuite, $reflection_test_class)
+            'tests' =>
+            $this->parseTestCases(
+                $testsuite,
+                $reflection_test_class,
+                $test_file_source
+            )
         ];
 
         $this->write_json($json_path, $output);
     }
 
-    private function write_json(string $json_path, array $output): void
-    {
-        $json = json_encode(
-            value: $output,
-            flags: JSON_THROW_ON_ERROR
-        );
-        file_put_contents($json_path, $json."\n");
-    }
-
-    private function getReflectionTestClass(string $test_class, string $test_file_path): ReflectionClass
-    {
-        require_once($test_file_path);
-        $class = new ReflectionClass($test_class);
-        return $class;
-    }
-
-    private function parseTestCases(SimpleXMLElement $testsuite, ReflectionClass $test_class): array
-    {
+    /**
+     * @param string[] $test_file_source
+     */
+    private function parseTestCases(
+        SimpleXMLElement $testsuite,
+        ReflectionClass $test_class,
+        array $test_file_source
+    ): array {
         $testcase_methods_by_name = [];
         foreach ($test_class->getMethods() as $method) {
             $testcase_methods_by_name[$method->getName()] = $method;
@@ -80,19 +79,24 @@ class Handler {
         foreach ($testsuite->testcase as $testcase) {
             $attrs = $testcase->attributes();
             $name = (string) $attrs['name'];
+            /** @var ReflectionMethod $method */
             $method = $testcase_methods_by_name[$name];
             $docblock = new Docblock($method->getDocComment());
-            
+
             $output = [
                 'name' => $name,
-                'status' => self::STATUS_PASS
+                'status' => self::STATUS_PASS,
+                'test_code' => $this->getTestCaseSource(
+                    method: $method,
+                    test_source: $test_file_source
+                )
             ];
 
             $task_id_tags = $docblock->getTags('task_id')->toArray();
             if ($task_id_tags) {
                 $tag = $task_id_tags[0];
                 $output['task_id'] = (int) ($tag->getDescription());
-            } 
+            }
 
             $testdoxi = $docblock->getTags('testdox')->toArray();
             if ($testdoxi) {
@@ -104,11 +108,11 @@ class Handler {
                 if ($name === 'system-out') {
                     $output['output'] = (string) $data;
                 } elseif ($name === 'error') {
-                    $output['status'] = self::STATUS_ERROR; 
-                    $output['message'] = (string) $data; 
+                    $output['status'] = self::STATUS_ERROR;
+                    $output['message'] = (string) $data;
                 } elseif ($name === 'failure') {
-                    $output['status'] = self::STATUS_FAIL; 
-                    $output['message'] = (string) $data; 
+                    $output['status'] = self::STATUS_FAIL;
+                    $output['message'] = (string) $data;
                 }
             }
 
@@ -116,5 +120,57 @@ class Handler {
         }
 
         return $testcase_outputs;
+    }
+
+    private function write_json(string $json_path, array $output): void
+    {
+        $json = json_encode(
+            value: $output,
+            flags: JSON_THROW_ON_ERROR
+        );
+        file_put_contents($json_path, $json . "\n");
+    }
+
+    private function getReflectionTestClass(
+        string $test_class,
+        string $test_file_path
+    ): ReflectionClass {
+        require_once($test_file_path);
+        $class = new ReflectionClass($test_class);
+        return $class;
+    }
+
+    private function getTestCaseSource(
+        ReflectionMethod $method,
+        array $test_source,
+    ): string {
+        $source_start_line = $method->getStartLine();
+        $source_end_line = $method->getEndLine();
+        $has_lines = !in_array(
+            needle: false,
+            haystack: [$source_start_line, $source_end_line],
+            strict: true
+        );
+        $test_lines = $has_lines
+            ? array_slice(
+                $test_source,
+                $source_start_line + 1,
+                $source_end_line - 2 - $source_start_line
+            )
+            : ['Unable to obtain test code.'];
+
+        $min_indent = PHP_INT_MAX;
+        foreach ($test_lines as $line) {
+            $indent = strlen($line) - strlen(ltrim($line));
+            if ($indent < $min_indent) {
+                $min_indent = $indent;
+            }
+        }
+
+        foreach ($test_lines as $idx => $line) {
+            $test_lines[$idx] = substr($line, $min_indent);
+        }
+
+        return implode("\n", $test_lines)."\n";
     }
 }
