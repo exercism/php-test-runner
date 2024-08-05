fccb20d2093cc56b80d891ae031239a1f5dbd3c8
Michael Kramer <m.kramer@mxp.de>
2024-08-05 10:56:54 +0200

    Normalize object id locally as in run-tests.sh

--- bin/run-locally.sh
+++ bin/run-locally.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 vendor/bin/phpunit --do-not-cache-result tests/"${1}"/*Test.php
+# Sync'ed from run-tests.sh - Normalize the object ID of `var_dump(new stdClass())`
+sed -i -E \
+    -e 's/(object\(stdClass\))(#[[:digit:]]+)/\1#79/g' \
+    results.json
 diff results.json tests/"${1}"/expected_results.json
